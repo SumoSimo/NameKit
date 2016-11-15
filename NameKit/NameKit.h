baa5b23213bccca919c29568396e8552aeea638c
Simon Tsai <simon.tsai.j@gmail.com>
2016-11-14 19:54:10 -0500

    Initial Commit

---
+++ NameKit/NameKit.h
@@ -0,0 +1,19 @@
+//
+//  NameKit.h
+//  NameKit
+//
+//  Created by Simon Tsai on 11/14/16.
+//  Copyright © 2016 Simon Tsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NameKit.
+FOUNDATION_EXPORT double NameKitVersionNumber;
+
+//! Project version string for NameKit.
+FOUNDATION_EXPORT const unsigned char NameKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NameKit/PublicHeader.h>
+
+
